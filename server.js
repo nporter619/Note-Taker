ef8e9b2f8cdff44818506386503d29cd7edbab4b
nporter <nporterwork@gmail.com>
2023-06-20 14:10:14 -0700

    added html routes and api routes and installed uuid

--- server.js
+++ server.js
@@ -8,3 +8,15 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
