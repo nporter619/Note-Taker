4cefeddcc34770f20821f13bc599d8c5854cf997
nporter <nporterwork@gmail.com>
2023-06-20 17:59:13 -0700

    added notes from my heroku generated app I worked on on thursday and added them over with extra

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
+// Import required modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+// Create an express app
 const app = express();
+// Set the PORT for the app to listen on
 const PORT = process.env.PORT || 3000;
 
+// Set express to use JSON parsing and static files from the 'public' directory
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// Route to serve the notes HTML file
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-
+// Route to get all notes
 app.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
@@ -19,11 +24,13 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
+// Import uuid to generate unique ids for notes
 const { v4: uuidv4 } = require('uuid');
 
+// Route to create a new note
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    newNote.id = uuidv4();
+    newNote.id = uuidv4();  // Generate a unique id for the new note
 
     fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
@@ -36,8 +43,9 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
+// Route to delete a note
 app.delete('/api/notes/:id', (req, res) => {
-    const noteId = req.params.id;
+    const noteId = req.params.id;  // Get the id of the note to be deleted
 
     fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
         if (err) throw err;
@@ -54,7 +62,8 @@ app.delete('/api/notes/:id', (req, res) => {
     });
 });
 
+// Catch-all route to serve the index HTML file
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
+// Start the app and listen on the PORT
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
-
